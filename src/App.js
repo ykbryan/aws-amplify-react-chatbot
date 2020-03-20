8c54b36d09f4d815c7e478ce0696600610599f5a
Bryan Chua <me@bryanchua.com>
2020-03-20 15:09:14 +0800

    Updating to use RestaurantBot instead

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Launcher } from 'react-chat-window'
+import { Launcher } from 'react-chat-window';
 import Amplify, { Interactions } from 'aws-amplify';
 import awsmobile from './aws-exports';
 
 Amplify.configure(awsmobile);
+Amplify.configure({
+  Interactions: {
+    bots: {
+      RestaurantAssistant: {
+        name: 'RestaurantAssistant',
+        alias: 'Staging',
+        region: 'us-east-1'
+      }
+    }
+  }
+});
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -18,64 +28,75 @@ class App extends Component {
     };
   }
 
-  _onMessageWasSent = (message) => {
+  _onMessageWasSent = message => {
+    try {
+      let currentMessageList = this.state.messageList;
+      if (this.state.hasResponded === false) {
+        currentMessageList.pop();
+      }
 
-    let currentMessageList = this.state.messageList
-    if (this.state.hasResponded === false) {
-      currentMessageList.pop()
+      this.setState({
+        tempMessageList: [...currentMessageList, message],
+        messageList: [
+          ...currentMessageList,
+          message,
+          {
+            author: 'lex',
+            type: 'text',
+            data: { text: '...' }
+          }
+        ],
+        hasResponded: false
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      // Set timeout to check if the temp message is working
+      // setTimeout(this.responseByLex(message.data.text), 3000)
+      this.responseByLex(message.data.text);
     }
+  };
+
+  responseByLex = async message => {
+    const response = await Interactions.send('RestaurantAssistant', message);
+    console.log(response);
 
     this.setState({
-      tempMessageList: [...currentMessageList, message],
-      messageList: [...currentMessageList, message, {
-        author: 'lex',
-        type: 'emoji',
-        data: { emoji: "💬" }
-      }],
-      hasResponded: false
-    })
-
-    // Set timeout to check if the temp message is working
-    // setTimeout(this.responseByLex(message.data.text), 3000)
-    this.responseByLex(message.data.text)
-  }
-
-  responseByLex = async (message) => {
-
-    const response = await Interactions.send("BookTrip_master", message);
-
-    this.setState({
-      messageList: [...this.state.tempMessageList, {
-        author: 'lex',
-        type: 'text',
-        data: { text: response.message }
-      }],
+      messageList: [
+        ...this.state.tempMessageList,
+        {
+          author: 'lex',
+          type: 'text',
+          data: { text: response.message }
+        }
+      ],
       hasResponded: true
-    })
-  }
+    });
+  };
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
         </header>
-        <div>
+        <div style={{ textAlign: 'left' }}>
           <Launcher
             agentProfile={{
-              teamName: 'react-chat-window',
-              imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+              teamName: 'Restaurant Chatter',
+              imageUrl:
+                'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
             }}
             onMessageWasSent={this._onMessageWasSent.bind(this)}
             messageList={this.state.messageList}
